495df3c48af98c7f0ee67df330da103c52ffd3f0
Anwar Sadat Ayub <anwar.rhsl@gmail.com>
2022-07-02 14:10:07 +0100

    create sidebar component

---
+++ components/Sidebar.js
@@ -0,0 +1,50 @@
+import Image from "next/Image";
+import SidebarMenuItem from "./SidebarMenuItem";
+import { DotsHorizontalIcon, HomeIcon} from "@heroicons/react/solid";
+import {BellIcon, BookmarkIcon, HashtagIcon, InboxIcon,ClipboardIcon, UserIcon, DotsCircleHorizontalIcon} from "@heroicons/react/outline";
+
+function Sidebar() {
+  return (
+    <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full">
+      {/* Twitter logo */}
+      <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1" >
+        <Image
+          alt="twittwer logo"
+          src="https://cdn.cms-twdigitalassets.com/content/dam/help-twitter/twitter_logo_blue.png.twimg.768.png"
+          width="50"
+          height={50}
+        ></Image>
+      </div>
+      {/* Menu */}
+        <div className="mt-4 mb-2.5 xl:items-start">
+            <SidebarMenuItem text="Home" Icon={HomeIcon} active />
+            <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
+            <SidebarMenuItem text="Notifications" Icon={BellIcon} />
+            <SidebarMenuItem text="Message" Icon={InboxIcon} />
+            <SidebarMenuItem text="Bookmark" Icon={BookmarkIcon} />
+            <SidebarMenuItem text="List" Icon={ClipboardIcon} />
+            <SidebarMenuItem text="Profile" Icon={UserIcon} />
+            <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon} />
+        </div>
+      {/* Button */}
+        <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">Tweet</button>
+
+      {/* Mini Profile */}
+        <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
+                <img 
+                    src="/images/profile.jpg" 
+                    alt="user-profile" 
+                    className="rounded-full h-10 w-10 xl:mr-2"
+                />
+                <div className="leading-5 hidden xl:inline">
+                    <h4 className="font-bold">Anwar Sadat Ayub</h4>
+                    <p className="text-gray-500">@momokid</p>
+                </div>
+                <DotsHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline"/>
+        </div>
+
+    </div>
+  );
+}
+
+export default Sidebar;
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,25 @@
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Twitter Cloe</title>
         <meta name="description" content="We are" />
         <link rel="icon" href="/favico.ico" />
       </Head>
 
-      <header>
-        <div className="container">
-          <div className="pis__top-nav">
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
-          </div>
-        </div>
-      </header>
-      <main></main>
+      <main className="flex min-h-screen max-w-7xl mx-auto ">
+        {/* Sidebar */}
+        <Sidebar />
 
-      <foter></foter>
+        {/* Feed */}
+
+        {/* Widget */}
+
+        {/* Model */}
+      </main>
     </div>
   );
 }
