b7cf05a97ab9fd08d5f9d52497241e7ecc81d546
Anwar Sadat Ayub <anwar.rhsl@gmail.com>
2022-07-02 21:34:22 +0100

    Added input section of the feed component

--- components/Feed.js
+++ components/Feed.js
@@ -1,5 +1,6 @@
 import { SparklesIcon } from '@heroicons/react/outline'
 import React from 'react'
+import Input from './Input'
 
 const Feed = () => {
   return (
@@ -10,6 +11,7 @@ const Feed = () => {
                 <SparklesIcon className='h-5' />
             </div>
         </div>
+        <Input />
     </div>
   )
 }
---
+++ components/Input.js
@@ -0,0 +1,28 @@
+import { EmojiHappyIcon, PhotographIcon } from "@heroicons/react/outline"
+
+const Input = () => {
+  return (
+    <div className="flex border-b border-gray-200 p-3 space-x-3">
+        <img 
+            src="/images/profile.jpg" 
+            alt="user-profile-image" 
+            className="h-11 w-11 rounded-full cursor-pointer hover:brightness-95"
+        />
+        <div className="w-full divide-y divide=gray-200">
+            <div className="">
+                <textarea className="w-full border-none focus:ring-0 text-lg placeholder:color-gray-700 tracking-wide min-h-[50px] " rows="2" placeholder="What's happening?"></textarea>
+            </div>
+            <div className="flex items-center justify-between pt-2.5">
+                <div  className="flex">
+                    <PhotographIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100"/>
+                    <EmojiHappyIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100"/>
+                </div>
+                <button className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-blod shadow-md hover:brightness-95 disabled:opacity-50" disabled>Tweet</button>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Input
